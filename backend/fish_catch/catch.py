cd9de60cd4337ab9287527d384c5069076b74b92
UFOTreeboy <jinn7431080@gmail.com>
2023-12-16 22:35:47 +0800

    更新作品2

--- backend/fish_catch/catch.py
+++ backend/fish_catch/catch.py
@@ -1,29 +1,31 @@
-from flask import Flask, render_template, Blueprint
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import numpy as np
+from flask import Flask, render_template
 from io import BytesIO
 import base64
 
 
-taiwan_catch = Blueprint('taiwan_catch', __name__)
+app = Flask(__name__)
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-
-@taiwan_catch.route("/")
-def fullscreen():
-    fig, ax = plt.subplots()
-    ax.plot(xpoints, ypoints)
-
-    img_buf = BytesIO()
-    fig.savefig(img_buf, format='png')
-    img_buf.seek(0)
-
-    encoded_img = base64.b64encode(img_buf.read()).decode('utf-8')
-
-    plt.close(fig)
-
-    return render_template('catch.html', chart=encoded_img)
+@app.route('/')
+def plot():
 
 
+    img = BytesIO()
 
+
+    a_dictionary = {"a": 4, "b": 20, "c": 6}
+    keys = a_dictionary.keys()
+    values = a_dictionary.values()
+    plt.bar(keys, values)
+
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('catch.html', plot_url=plot_url)
+
+if __name__ == '__main__':
+    app.run()
