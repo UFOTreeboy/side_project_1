ec9b6f811c359e12287f26dd6e35e839250800e7
UFOTreeboy <jinn7431080@gmail.com>
2024-02-26 22:55:25 +0800

    新增CRUD

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # side_project_1
+
+
+p1.jpg圖片來源
+https://www.agriharvest.tw/archives/11055
+
+p2.jpg圖片來源
+https://www.agriharvest.tw/archives/60010
+
+p3.jpg圖片來源
+https://www.agriharvest.tw/archives/47630
--- view/information/aboutme.py
+++ view/information/aboutme.py
@@ -1,4 +1,5 @@
-from flask import  render_template, Blueprint
+from flask import  render_template, Blueprint, request, redirect, url_for
+from database.database import connect_to_mongodb
 
 aboutme = Blueprint('aboutme',__name__,
                     template_folder= 'templates',)
@@ -7,6 +8,25 @@ aboutme = Blueprint('aboutme',__name__,
 def about_me():
     return render_template('information/aboutme.html')
 
-@aboutme.route("/call_me",methods=['GET'])
+@aboutme.route("/call_me",methods=['POST'])
 def call_me():
-    return render_template('information/callme.html')
+    database_name = 'treeboy'
+    collection_name = 'a_users'
+    data_collection = connect_to_mongodb(database_name, collection_name)
+
+    name = request.form.get('name')
+    profession = request.form.get('profession')
+    text = request.form.get('text')
+
+    
+    if data_collection.name != collection_name:
+        data_collection = connect_to_mongodb(database_name, collection_name)
+
+        new_data = {
+        "name": name,
+        "profession": profession,
+        "text": text
+        }
+    data_collection.insert_one(new_data)
+
+    return redirect(url_for('aboutme.about_me'))
