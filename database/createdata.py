bc65cc66bf3ba7f58404980dd27534904d012aee
UFOTreeboy <jinn7431080@gmail.com>
2024-02-28 20:27:00 +0800

    增加MongoDB Atlas之資料庫存取

--- database/createdata.py
+++ database/createdata.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def create_collection():
     database_name = 'treeboy'
     collection_name = 'new_users'
-    data_collection = connect_to_mongodb(database_name, collection_name)
+    db = connect_to_mongodb(database_name, collection_name)
 
-    if collection_name not in data_collection.database.list_collection_names():
-        collection = data_collection.database.create_collection(collection_name)
+    if collection_name not in db.database.list_collection_names():
+        collection = db.database.create_collection(collection_name)
         collection.create_index([('name')], unique=True)
         collection.create_index([('profession')])
         collection.create_index([('text')])
--- view/information/callme.py
+++ view/information/callme.py
@@ -1,12 +1,17 @@
-from flask import  render_template, Blueprint
+from flask import  render_template, Blueprint,request
+from database.database import connect_to_mongodb
 
 
 callme = Blueprint('callme',__name__,
                     template_folder= 'templates',)
 
-
+db = connect_to_mongodb('treeboy', 'new_users')
 
 @callme.route("/callme",methods=['GET','POST'])
 def call_me():
-
+    if request.method=='POST':
+        name = request.form['name']
+        profession = request.form['profession']
+        text = request.form["text"]
+        db.insert_one({'name':name , 'profession':profession , 'text': text})
     return render_template('information/callme.html')
