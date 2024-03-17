f5ebac46c508a91a08bbaff33e487a7c635d674c
UFOTreeboy <jinn7431080@gmail.com>
2024-03-18 00:04:32 +0800

    新增大數據

--- view/fishdata/fishy_worker.py
+++ view/fishdata/fishy_worker.py
@@ -1,17 +1,33 @@
 from flask import  render_template, Blueprint
+from database.database import connect_to_mongodb
 import base64
 from io import BytesIO
 from matplotlib.figure import Figure
+import seaborn as sns
+import pandas as pd
+
 
 fishy_worker = Blueprint('fishy_worker',__name__,
                 template_folder= 'templates',
                 static_folder='static')
 
+data_collection = connect_to_mongodb('treeboy', 'fisky_work_ocean')
+cl = pd.DataFrame(list(data_collection.find()))
+
+
 @fishy_worker.route("/",methods=['GET'])
 def worker():
-    fig = Figure()
+    
+
+
+
+
+
+    fig = Figure(figsize=(10, 5.2))
     ax = fig.subplots()
-    ax.plot([1, 2])
+    x_sorted = cl['date']
+    y_sorted = cl['value']
+    ax.plot(x_sorted, y_sorted)
     buf = BytesIO()
     fig.savefig(buf, format="png")
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
